1c79040b08cc23f5aaf4c4ade88a63cd20297771
jay3dave <jay3dave@gmail.com>
2013-05-09 19:03:58 -0400

    changed useraccount.php for searchfeature and header.php

--- useraccount.php
+++ useraccount.php
@@ -6,55 +6,10 @@ if(isset($_SESSION['userlogin'])){
  else {
     header("location:index.php");            
 }
+include "header.php";
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="iso-8859-1" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Advertisement Portal</title>
-<link href="css/style.css" rel="stylesheet">
-<!--[if lt IE 9]>
-      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-      <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-<![endif]-->
-</head>
-<body>
-    <div id="container">
-        <div id="header">
-            <a href="#">
-                <a href="index.php"><div id="logo">
-                
-                </div></a>
-            </a>
-            <nav id="top-right-nav">
-                 <ul>
-                    <li><a href="logout.php">Logout | </a></li>
-                    <li><a href="register.php">Register | </a></li>
-                    <li><a href="help.php">Help</a></li>
-                </ul>    
-            </nav>
-            <div id="find-us">
-                <ul>
-                    <li><a href="#"><img src="images/Facebook.png" alt="Team Watermelon" class="imgs" /></a></li>
-                    <li><a href="#"><img src="images/Twitter.png" alt="Team Watermelon" class="imgs" /></a></li>
-                    <li><a href="#"><img src="images/Youtube.png" alt="Team Watermelon" class="imgs" /></a></li>
-                </ul>
-            </div>
-      
-            <nav id="main-nav">
-                <ul>
-                    <li><a href="#">Post Ad</a></li>
-                    <li><a href="#">Account </a></li>
-                    <li><a href="#">Hot-Deals</a></li>
-                    <li><a href="#">Advanced Search</a></li>
-                    <li><a href="#">About Us</a></li>
-                </ul>
-            </nav>
-        </div>
-        <div id="search-bar-home">
-            
-        </div>
+
 
 <?php
     $email = $_SESSION["userlogin"];
@@ -80,7 +35,7 @@ if(isset($_SESSION['userlogin'])){
     $ans2 = $row['ans2'];  
     //echo $img;
 }
-    
+    include "searchfeature.php";
 ?>
 
 <div id="main">
